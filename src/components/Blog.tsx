dbaa4109c54f4a52105a9dd8c75b39b8c63af1ee
= <115790519+sanakulovme@users.noreply.github.com>
2025-07-04 15:56:33 +0500

    update

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Calendar, ArrowRight } from "lucide-react";
 import { useLanguage } from "../context/LanguageContext";
+// import  folders images
+
 
 const Blog: React.FC = () => {
   const { t, language } = useLanguage(); // Ensure `language` is destructured from the context
@@ -8,7 +10,7 @@ const Blog: React.FC = () => {
   const blogPosts = [
     {
       id: 1,
-      image: "/src/images/blog1.png",
+      image: "../src/images/blog1.png",
       date: "2024-01-15",
       title: {
         en: "Standard penetration test (SPT) is a measurement of the number of blows N required for penetration (passage of the sampling device into the soil or rock).",
@@ -18,7 +20,7 @@ const Blog: React.FC = () => {
     },
     {
       id: 2,
-      image: "/src/images/Рисунок11.jpg",
+      image: "../src/images/Рисунок11.jpg",
       date: "2024-01-10",
       title: {
         en: "Working moment of performing electrical exploration works using the Vertical Electrical Sounding (VES) method.",
@@ -28,7 +30,7 @@ const Blog: React.FC = () => {
     },
     {
       id: 3,
-      image: "/src/images/Рисунок15.jpg",
+      image: "../src/images/Рисунок15.jpg",
       date: "2024-01-05",
       title: {
         en: "Chemical analysis of natural water and aqueous extract of soils for the needs of engineering and geological surveys.",
@@ -38,7 +40,7 @@ const Blog: React.FC = () => {
     },
     {
       id: 4,
-      image: "/src/images/Рисунок18.png",
+      image: "../src/images/Рисунок18.png",
       date: "2024-01-05",
       title: {
         en: "Laboratory sieves for mechanical analysis for sand grain size distribution",
@@ -48,7 +50,7 @@ const Blog: React.FC = () => {
     },
     {
       id: 4,
-      image: "/src/images/Рисунок17.png",
+      image: "../src/images/Рисунок17.png",
       date: "2024-01-05",
       title: {
         en: "Laboratory studies of soils are carried out to determine their composition, condition, physical and mechanical properties.",
@@ -58,7 +60,7 @@ const Blog: React.FC = () => {
     },
     {
       id: 5,
-      image: "/src/images/Рисунок12.jpg",
+      image: "../src/images/Рисунок12.jpg",
       date: "2024-01-05",
       title: {
         en: "Working moment of impact production for excitation of longitudinal and transverse waves",
